8e2bbf50c0b94c275e39a613a834b4bff13b698a
ilhan acim <0xilhan@192.168.1.16>
2026-04-07 05:30:05 +0300

    force fix

--- api/frame.js
+++ api/frame.js
@@ -1,29 +1,15 @@
 module.exports = (req, res) => {
-  if (req.method === "GET") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end(`
-      <html>
-        <head>
-          <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="https://picsum.photos/600/400" />
-          <meta property="fc:frame:button:1" content="Check In" />
-          <meta property="fc:frame:post_url" content="https://base-frame-neon.vercel.app/api/frame" />
-        </head>
-      </html>
-    `);
-  }
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
 
-  if (req.method === "POST") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end(`
-      <html>
-        <head>
-          <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="https://picsum.photos/600/400?text=Success" />
-        </head>
-      </html>
-    `);
-  }
+  res.end(`
+    <html>
+      <head>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://picsum.photos/600/400" />
+        <meta property="fc:frame:button:1" content="Check In" />
+        <meta property="fc:frame:post_url" content="https://base-frame-neon.vercel.app/api/frame" />
+      </head>
+    </html>
+  `);
 };
